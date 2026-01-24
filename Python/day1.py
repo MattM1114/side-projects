4dd5942719a46915e847b9593cd9727e804c6640
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2026-01-24 14:32:53 +0200

    day 1
    day 1 100 days of programming practices

---
+++ Python/day1.py
@@ -0,0 +1,8 @@
+import random
+
+messages = ["Python will change your life!",
+            "you are going to be a great programmer!",
+            "Keep coding every day!",
+            "AI engineers start like this!",
+]
+print(random.choice(messages))
